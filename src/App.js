239109aa33b88bc52e06a78b359622a4e16cf7ee
“ALECRAFT-BOT” <waltertoscano013@gmail.com>
2021-10-12 09:54:03 -0500

    tercer cambio

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import Final from './Final';
 
 
 
-
+const Modo= (props)=>{
+return <h1>{props.promo}</h1>
+}
 
 
 
 const App = ()=> {
-
+const saludo= "hola a todos";
   return (
     <div className="App">
+				<Modo promo={saludo}  />
 				<Mensajes color='red' mens="hola a todos amigos"/>
 				<Mensajes color='yellow' mens="hola a todos enemigos"/>
 				<Final otro="ustedes si saben reacts" />
